bc15879442db02784a03e158fdaa56504f4837a8
Frank <frank.boeddeker@gmx.net>
2015-04-21 17:23:04 +0200

    changes function names for timer directive

--- app/core/scripts/components/timer/timer_controller.js
+++ app/core/scripts/components/timer/timer_controller.js
@@ -3,11 +3,31 @@
 
     angular
         .module('app')
-        .controller('TimerController', timerController);
+        .controller('TimerController', TimerController);
 
-    function timerController() {
+    function TimerController($timeout) {
         var vm = this;
 
+        // 25 minutes
+        vm.timeRemaining = 25 * 1000 * 60;
+        vm.disableButton = true;
+
+        vm.countdown = countdown;
+        vm.pauseCountdown = pauseCountdown;
+
+
+        function countdown () {
+            console.log("testLog");
+            vm.disableButton = false;
+            $timeout(function () {
+                vm.timeRemainsing = vm.timeRemainsing - 1000;
+            }, 1000);
+        }
+
+        function pauseCountdown () {
+            vm.disableButton = true;
+            $timeout.cancel();
+        }
 
     }
 
