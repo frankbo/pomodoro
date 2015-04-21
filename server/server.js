daf39509c287f0e29b51de69442e06e808c20d6c
Frank <frank.boeddeker@gmx.net>
2015-04-21 23:33:06 +0200

    added socket messages to broadcast time remaining and status of buttons

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
-(function() {
+(function () {
+    'use strict';
+
     var express = require('express');
     var app = express();
-    var server = app.listen(3000, function(){
+    var server = app.listen(3000, function () {
         console.log('listening on *:3000');
     });
     var io = require('socket.io').listen(server);
@@ -9,15 +11,19 @@
     app.use(express.static(__dirname + '/../app'));
     app.use('/bower_components', express.static(__dirname + '/../bower_components'));
 
-    app.get('/', function(req, res) {
+    app.get('/', function (req, res) {
         res.sendFile('index.html', { root: __dirname + '/../app/' });
     });
 
-    io.on("connection", function(socket) {
-        socket.on("timeCtrl:timeRemaining", function(remainingTime) {
-           console.log(remainingTime);
+    io.on('connection', function (socket) {
+        socket.on('timer:remaining', function (remainingTime) {
+            io.emit('timer:remaining', remainingTime);
+        });
+
+        socket.on('timer:buttonChange', function (buttonChange) {
+            io.emit('timer:buttonChange', buttonChange);
         });
     });
 
     module.exports = app;
-})();
+}());
