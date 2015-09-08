31647c6fadfaa8ddf76ca081145553bbe9a43b2f
Frank <frank.boeddeker@gmx.net>
2015-09-08 22:21:11 +0200

    add scss support and watcher for scss

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,8 +4,19 @@ var path = require('path');
 
 module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-express');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    //require('load-grunt-tasks')(grunt);
 
     grunt.initConfig({
+        // Watches files for changes and runs tasks based on the changed files
+        watch: {
+            sass: {
+                files: 'app/style/scss/**/*.{scss,sass}',
+                tasks: ['sass']
+            }
+        },
+
         express: {
             options: {
                 port: 3000,
@@ -18,10 +29,28 @@ module.exports = function (grunt) {
                     base: 'server'
                 }
             }
-        }
+        },
 
-        //scss {}
+        // Compiles Sass to CSS and generates necessary files if requested
+        sass: {
+            options: {
+                sourceMap: false,
+                includePaths: ['app/bower_components']
+            },
+            server: {
+                options: {
+                    sourceMap: false
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'app/style/scss',
+                    src: ['**/*.scss'],
+                    dest: 'app/style/css',
+                    ext: '.css'
+                }]
+            }
+        }
     });
 
-    grunt.registerTask('default', ['express']);
+    grunt.registerTask('serve', ['express']);
 };
