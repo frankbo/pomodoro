941f6838f77d857e232517d5395ee09aacd072ec
Frank <frank.boeddeker@gmx.net>
2015-09-22 07:55:07 +0200

    send new channel to backend instead of direclty adding it to the local scope

--- app/core/scripts/components/channel/add/add_channel_controller.js
+++ app/core/scripts/components/channel/add/add_channel_controller.js
@@ -5,18 +5,22 @@
         .module('app')
         .controller('AddChannelController', AddChannelController);
 
-    function AddChannelController(store) {
+    function AddChannelController(store, resource) {
         var vm = this;
         vm.channelName = '';
         vm.channelList = store.getChannelList();
 
         vm.addChannel = function () {
+            var newChannel;
             if (!vm.channelName) { return; }
 
-            vm.channelList.push({
+            newChannel = {
                 id: vm.channelList.length + 1,
                 name: vm.channelName
-            });
+            };
+
+            resource.addChannel(newChannel);
+            // TODO See if vm is automagically updated after receiving the answer from the endpoint
         };
     }
 }());
